0b58b51d0b31f3eb37af70c38b2da55cf9852b36
Parikshit-Wayal <parikshitwayal3@gmail.com>
2025-04-23 09:34:38 +0000

    ok

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 
+//hello from server
+
 // Respond with "Hello World" when a GET request is made to the root "/"
 
 app.get('/', (req, res) => {
